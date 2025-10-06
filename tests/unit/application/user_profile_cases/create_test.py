353335a07de1fb9ab6a47517a79215455253a83f
Pedro Maximo <pedromaximocc@gmail.com>
2025-10-06 14:39:53 -0300

    feat-test: adicionando um mock de repositorio para testar casos de excecao tratadas que esperamos devido as nossas regras de negocio, adicionando o caso de um adim tentar adicionar um usuario que ja esteja cadastrado e ativo no nosso banco e esperando a excecao correta

--- tests/unit/application/user_profile_cases/create_test.py
+++ tests/unit/application/user_profile_cases/create_test.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-
+from src.shared.custom_exceptions import UniqueViolation
 import pytest
-
+from unittest.mock import create_autospec, MagicMock
+from src.domain.repositories.user_profile_repository import IUserProfileRepository
 from tests.unit.double.mongo_user_profile_repository_spy import MongoUserProfileRepositorySpy as Repository
 from src.domain.use_cases.user_profile.create import CreateUserProfileInput, CreateUserProfileOutput
 from src.application.use_cases.user_profile_cases.create import CreateUserProfileUseCase
+from src.api.advices.apiError import BadRequest
+
 
 @pytest.mark.unit
 class TestCreateUserProfile:
@@ -14,6 +17,11 @@ class TestCreateUserProfile:
         return Repository()
 
 
+    @pytest.fixture()
+    def setup_mocked_repository(self) -> MagicMock:
+        return create_autospec(IUserProfileRepository, spec_set=True)
+
+
     @pytest.fixture
     def setup_user_profile_input(self) -> CreateUserProfileInput:
         mock_user_profile_input = CreateUserProfileInput(email= "pedromaximocc@gmail.com", can_use_ai_agent=False, can_access_sensitive_information=True)
@@ -21,17 +29,22 @@ class TestCreateUserProfile:
 
 
     @pytest.fixture
-    def setup_use_case(self, setup_repository: Repository) -> CreateUserProfileUseCase:
+    def setup_use_case_with_spy(self, setup_repository: Repository) -> CreateUserProfileUseCase:
         return CreateUserProfileUseCase(setup_repository)
 
 
-    def test_create_user_profile_success(self, setup_user_profile_input: CreateUserProfileInput, setup_repository: Repository,
-                                         setup_use_case: CreateUserProfileUseCase) -> None:
+    @pytest.fixture
+    def setup_use_case_with_mock(self, setup_mocked_repository: MagicMock) -> CreateUserProfileUseCase:
+        return CreateUserProfileUseCase(setup_mocked_repository)
+
+
+    def test_create_a_valid_user_profile_then_return_created_user(self, setup_user_profile_input: CreateUserProfileInput, setup_repository: Repository,
+                                         setup_use_case_with_spy: CreateUserProfileUseCase) -> None:
         # Assign
         mocked_input = setup_user_profile_input
         repository = setup_repository
         # Act
-        created_user = setup_use_case.execute(userDataInput=setup_user_profile_input)
+        created_user = setup_use_case_with_spy.execute(userDataInput=setup_user_profile_input)
 
 
         # Assert
@@ -46,3 +59,16 @@ class TestCreateUserProfile:
         assert any(call[0] == "insert" for call in repository.calls)
         assert repository.last_saved is not None
         assert repository.last_saved.id == created_user.id
+
+
+    def test_create_an_existent_active_user_then_raise_bad_request_exception(self, setup_mocked_repository: MagicMock, setup_user_profile_input: CreateUserProfileInput,
+                                                                             setup_use_case_with_mock: CreateUserProfileUseCase) -> None:
+        # Assign
+        setup_mocked_repository.insert.side_effect = UniqueViolation("email", "pedromaximocc@gmail.com")
+
+        # Act
+        with pytest.raises(BadRequest):
+            setup_use_case_with_mock.execute(userDataInput=setup_user_profile_input)
+
+        # Assert
+        setup_mocked_repository.insert.assert_called_once()
